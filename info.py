507dfac52ed54691fa7076224fb71e79d36c0dd7
nikhilvishwa7 <123230101+nikhilvishwa7@users.noreply.github.com>
2024-04-08 17:58:03 +0530

    Update info.py

--- info.py
+++ info.py
@@ -22,7 +22,7 @@ API_ID = int(environ.get('API_ID', '28625475'))
 
 API_HASH = environ.get('API_HASH', 'da4894b36ca60168ba283519ed551606')
 
-bot_token = os.environ["BOT_TOKEN"]
+BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
