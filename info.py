e324ebee52021718ddb596979b84429b20cce2cb
nikhilvishwa7 <123230101+nikhilvishwa7@users.noreply.github.com>
2024-04-08 17:45:53 +0530

    Update info.py

--- info.py
+++ info.py
@@ -37,7 +37,7 @@ CLOSE_IMG = (environ.get('CLOSE_IMG', 'https://telegra.ph/file/7fdabba7c2b10137b
 JOIN_IMG = (environ.get('JOIN_IMG', 'https://telegra.ph/file/7fdabba7c2b10137beffd.jpg')).split()
                     
 # Admins, Log, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5149183428 5238367049').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
